108c6f9e32f1225bf0c0258b1cf10918912ca332
Ahammad204 <Ahammadcode@gmail.com>
2023-10-02 15:53:49 +0600

    Final

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Live Link
+
+Live Link: https://luminous-gaufre-8844c3.netlify.app/
+
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
     return (
-        <div className='text-center'>
-            <h1 className='text-[400px] font-bold'>
+        <div className='text-center mt-52'>
+            <h1 className='text-6xl font-bold'>
                 404
             </h1>
 
             <h2 className='text-5xl font-bold'>
                 NOT FOUND
             </h2>
-
+            <Link to="/"><button className="btn btn-secondary mt-5">Home</button></Link>
         </div>
     );
 };
